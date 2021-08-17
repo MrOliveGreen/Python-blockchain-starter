6ea6024a4110746239b046a1b6a71025e0609f42
OliveSonDev <olivesondev@gmail.com>
2021-08-18 06:07:30 +0900

    update: proof_of_work & hash

--- blockchain.py
+++ blockchain.py
@@ -28,7 +28,20 @@ class Blockchain:
     def get_previous_hash(self):
         return self.chain[-1]
     
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
     
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys = True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
         
 # Part 2 - Mining our Blockchain
 
