7cc7c89c1163b7e6d52cf7f364aa9f8019ca5649
OliveSonDev <olivesondev@gmail.com>
2021-08-18 07:38:58 +0900

    update: create blockchain & mine, get chain

--- blockchain.py
+++ blockchain.py
@@ -59,4 +59,30 @@ class Blockchain:
             block_index += 1
         return True
 # Part 2 - Mining our Blockchain
+# Creating a Web App
+app = Flask(__name__)
+
+# Creating a Blockchain
+blockchain = Blockchain()
+
+# Mining a new block
+@app.route('/mine_block', methods = ['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message': 'Congratulations, you just mined a block!',
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']}
+    return jsonify(response), 200
+
+# Getting the full Blockchain
+@app.route('get_chain', methods = ['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain, 'length': len(blockchain.chain)}
+    return jsonify(response), 200
 
