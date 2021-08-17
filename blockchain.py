fd8e7b9c60efb4745619bbf714d0ed5f8e7d113d
OliveSonDev <olivesondev@gmail.com>
2021-08-18 01:31:57 +0900

    update: add gitignore, start blockchain

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,21 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Aug 18 01:09:17 2021
+# Module 1 - Create a Blockchain
 
-@author: Olive
-"""
+# To be installed:
+# Flask==0.12.2: pip install Flask==0.12.2
+# Postman HTTP Client: https://www.getpostman.com/
+
+# Importing the libraries
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+# Part 1 - Building a BLockchain
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_has = '0')
+        
+# Part 2 - Mining our Blockchain
 
